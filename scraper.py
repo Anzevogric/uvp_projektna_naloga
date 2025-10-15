4f17fd6ed1237fb815672756ff705bdd58344c1e
AnzeVogric <av91153@student.uni-lj.si>
2025-10-15 18:52:17 +0200

    analiza2

--- scraper.py
+++ scraper.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 
 
 def prenesi_stran():
-    """Prenese spletno stran 'Best Books Ever' z Goodreads in jo shrani kot HTML."""
     url = "https://www.goodreads.com/list/show/1.Best_Books_Ever"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -20,13 +19,12 @@ def prenesi_stran():
         pot = os.path.join("podatki", "goodreads_stran.html")
         with open(pot, "w", encoding="utf-8") as f:
             f.write(odgovor.text)
-        print("Stran uspešno prenesena in shranjena kot goodreads_stran.html")
+        print("Stran prenesena in shranjena v goodreads_stran.html")
     else:
-        print(f"Napaka pri pridobivanju strani (status {odgovor.status_code})")
+        print(f"Napaka, (status {odgovor.status_code})")
 
 
 def izlusci_knjige():
-    """Prebere shranjeno Goodreads HTML stran in izlušči podatke o knjigah ter jih shrani v CSV."""
     pot = os.path.join("podatki", "goodreads_stran.html")
 
     with open(pot, "r", encoding="utf-8") as f:
@@ -55,9 +53,8 @@ def izlusci_knjige():
         writer.writeheader()
         writer.writerows(knjige)
 
-    print("Podatki shranjeni v knjige.csv")
+    print("Shranjeno v knjige.csv")
 
 
 if __name__ == "__main__":
-    # prenesi_stran()  # če želiš ponovno prenesti HTML
     izlusci_knjige()
